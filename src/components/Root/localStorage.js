efdfe6af48f1d7cad47a22d5d29adf27068da28d
yokselzok <yokselzok@gmail.com>
2019-01-03 18:19:52 +0300

    Rename localStorage item

--- src/components/Root/localStorage.js
+++ src/components/Root/localStorage.js
@@ -1,9 +1,9 @@
 export const saveState = (state) => {
-  localStorage.setItem('sf-state', JSON.stringify(state));
+  localStorage.setItem('gradientmap-state', JSON.stringify(state));
 };
 
 export const getState = () => {
-  const storageValue = localStorage.getItem('sf-state');
+  const storageValue = localStorage.getItem('gradientmap-state');
 
   if (storageValue) {
     return JSON.parse(storageValue);
